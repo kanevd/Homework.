a444b753837751caa54f2c307bb97ede25462e99
kanevd <125911187+kanevd@users.noreply.github.com>
2023-02-27 14:09:32 +0200

    Update Calculator.py

--- Python calculator/Calculator.py
+++ Python calculator/Calculator.py
@@ -1,5 +1,6 @@
+#define functions for the different operations
 def Add(num1,num2):
-    return num1+num2
+    return num1 + num2
  
 def Substract(num1,num2):
     return num1 - num2
@@ -9,7 +10,20 @@ def Multiply(num1,num2):
  
 def Divide(num1,num2):
     return num1 / num2
+
+#function for a new calculation
+def repeat():
+    choice2 = input("Would you like to do a new calculation? Enter yes or no: ")
+    if choice2 == "yes":
+        calculate()
+    elif choice2 == "no":
+        print("Bye.")
+        return
+    else:
+        print("Invalid input")
+        repeat()
  
+#calculate menu
 def calculate():
     operation = input('''
 Select operation:
@@ -20,45 +34,37 @@ Select operation:
 Enter a choice:
 ''')
  
+ #proceeding with the calculation if one of the correct operation is selected
     if operation == "1" or operation == "2" or operation == "3" or operation == "4":
         while True:
             try:
                 num1 = float(input("Enter first number: "))
                 num2 = float(input("Enter second number: "))
-                if operation == "4" and num1 <= 0 or num2 <= 0:
-                    print("division by zero error handling");
+                if operation == "4" and (num1 <= 0 or num2 <= 0):
+                    print("division by zero error handling"); #division by zero check
                     repeat()
- 
-                if operation == "1":
+                    return
+                elif operation == "4" and (num1 > 0 and num2 > 0):
+                    print(f"Result is: {num1} / {num2} = {Divide(num1,num2)}")
+                elif operation == "1":
                     print(f"Result is: {num1} + {num2} = {Add(num1,num2)}")
                 elif operation == "2":
                     print(f"Result is: {num1} - {num2} = {Substract(num1,num2)}")
                 elif operation == "3":
                     print(f"Result is: {num1} * {num2} = {Multiply(num1,num2)}")
-                elif operation == "4":
-                    print(f"Result is: {num1} / {num2} = {Divide(num1,num2)}")
-                else:
-                    print("Invalid input")
  
+                else: #checking for invalid inputs below
+                    print("Invalid input")
+                    repeat()
+                    return
                 repeat()
+                return
             except ValueError:
                 print("Invalid input")
                 repeat()
                 break
- 
     else:
         print("Invalid input")
         repeat()
- 
-def repeat():
-    choice2 = input("Would you like to do a new calculation? Enter yes or no: ")
-    if choice2 != "yes" or choice2 != "no":
-        print("Invalid input")
-        repeat()
- 
-    if choice2 == "yes":
-        calculate()
-    elif choice2 == "no":
-        print("Bye.")
  
 calculate()
